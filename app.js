68c71f8de1f57418269e7c7fabb355c15327f65e
Kurs <sravani.basineni@gmail.com>
2019-05-03 12:59:38 +0200

    second commit

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 var Addition=require('./Addition.js');
 console.log(Addition.AddNumber(1,2));
+
+var localTutor=require('./NodeTutorial.js');
+localTutor.NodeTutorial();
+localTutor.NodeTutorial.pTutor();
+
+Or use this code
+var tut = new localTutor.NodeTutorial();  // Create and save object
+tut.pTutor();  // Call function on object
